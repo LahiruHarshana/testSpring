d48bb35b051cfe5a457db34c3a9a1d6ebe62fca7
LahiruHarshana <lharshana2002@gmail.com>
2024-02-02 15:55:29 +0530

    first commit

--- src/main/java/lk/ijse/AppConfig.java
+++ src/main/java/lk/ijse/AppConfig.java
@@ -1,10 +1,11 @@
 package lk.ijse;
 
+import lk.ijse.bean.ChuBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackageClasses = ChuBean.class)
 public class AppConfig {
     public AppConfig() {
         System.out.println("AppConfig Instantiated");
---
+++ src/main/java/lk/ijse/bean/hello.java
@@ -0,0 +1,11 @@
+package lk.ijse.bean;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class hello {
+    hello(){
+        System.out.println("Hello Bean Instantiated");
+    }
+}
